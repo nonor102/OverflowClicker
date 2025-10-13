a7c37a22a289add26e55ee7f7685103fb22f1a04
nonor102 <105913423+nonor102@users.noreply.github.com>
2025-10-13 19:20:20 +0900

    UI mawari

--- OverflowClicker/Assets/Scripts/UI.cs
+++ OverflowClicker/Assets/Scripts/UI.cs
@@ -1,36 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] private Button click;
-    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI alphaFactorText;
+    [SerializeField] private Canvas alpha;
+    [SerializeField] private Canvas beta;
+    [SerializeField] private Canvas gamma;
+    [SerializeField] private Canvas delta;
+    [SerializeField] private Canvas epsilon;
+    [SerializeField] private Canvas settings;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ‰Šúó‘Ô‚Åalphaƒpƒlƒ‹•\Ž¦
+        AlphaCanvasEnable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "AF: " + GameManager.Instance.AlphaFactorForDisplay;
+        alphaFactorText.text = "AF: " + GameManager.Instance.AlphaFactorForDisplay;
         if (GameManager.Instance.IsAlphaOverflowCollapsed && GameManager.Instance.AlphaOverflowCount > 0)
         {
-            scoreText.text += "\n" + "(Overflow: " + GameManager.Instance.AlphaOverflowCount + ")";
+            alphaFactorText.text += "\n" + "(Overflow: " + GameManager.Instance.AlphaOverflowCount + ")";
         }
     }
 
-    public void OnSbyteButtonClicked()
+    public void OnAlphaFactorButtonClicked()
     {
         GameManager.Instance.UpdateAlphaFactor();
     }
-
-    /*void ScoreDisplayUpdate()
+    public void AlphaCanvasEnable()
     {
-        scoreText.text = "" + GameManager.Instance.AlphaFactorForDisplay;
-    }*/
+        if(alpha != null)
+        {
+            alpha.enabled = true;
+            beta.enabled = false;
+            gamma.enabled = false;
+            delta.enabled = false;
+            epsilon.enabled = false;
+            settings.enabled = false;
+            Debug.Log("seeing alpha");
+        }
+    }
+    public void BetaCanvasEnable()
+    {
+        if(beta != null)
+        {
+            beta.enabled = true;
+            alpha.enabled = false;
+            gamma.enabled = false;
+            delta.enabled = false;
+            epsilon.enabled = false;
+            settings.enabled = false;
+            Debug.Log("seeing beta");
+        }
+    }
+    public void GammaCanvasEnable()
+    {
+        if(gamma != null)
+        {
+            gamma.enabled = true;
+            alpha.enabled = false;
+            beta.enabled = false;
+            delta.enabled = false;
+            epsilon.enabled = false;
+            settings.enabled = false;
+            Debug.Log("seeing gamma");
+        }
+    }
+    public void DeltaCanvasEnable()
+    {
+        if(delta != null)
+        {
+            delta.enabled = true;
+            alpha.enabled = false;
+            beta.enabled = false;
+            gamma.enabled = false;
+            epsilon.enabled = false;
+            settings.enabled = false;
+            Debug.Log("seeing delta");
+        }
+    }
+    public void EpsilonCanvasEnable()
+    {
+        if(epsilon != null)
+        {
+            epsilon.enabled = true;
+            alpha.enabled =false;
+            beta.enabled = false;
+            gamma.enabled = false;
+            delta.enabled = false;
+            settings.enabled = false;
+            Debug.Log("seeing epsilon");
+        }
+    }
+    public void SettingsCanvasEnable()
+    {
+        if(settings != null)
+        {
+            settings.enabled = true;
+            alpha.enabled = false;
+            beta.enabled = false;
+            gamma.enabled = false;
+            delta.enabled = false;
+            epsilon.enabled = false;
+            Debug.Log("seeing settings");
+        }
+    }
+
 }
