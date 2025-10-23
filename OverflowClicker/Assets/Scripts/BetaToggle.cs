e6f256edfc61a4d1bc0142ef853baf91d3aa6e59
nonor102 <105913423+nonor102@users.noreply.github.com>
2025-10-23 11:48:02 +0900

    toggle fix

--- OverflowClicker/Assets/Scripts/BetaToggle.cs
+++ OverflowClicker/Assets/Scripts/BetaToggle.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class BetaToggle : MonoBehaviour
 {
+    [SerializeField] private Toggle upgrade;
     [SerializeField] private Toggle amplification;
     [SerializeField] private Toggle mission;
     [SerializeField] private Toggle bank;
@@ -12,57 +13,18 @@ public class BetaToggle : MonoBehaviour
 
     void Start()
     {
-        amplification.onValueChanged.AddListener(VaridateAmplificationToggle);
-        mission.onValueChanged.AddListener(VaridateMissionToggle);
-        bank.onValueChanged.AddListener(VaridateBankToggle);
-        revolution.onValueChanged.AddListener(VaridateRevolutionToggle);
+        upgrade.interactable = true;
+        amplification.interactable = false;
+        mission.interactable = false;
+        bank.interactable = false;
+        revolution.interactable = false;
     }
 
-    private void VaridateAmplificationToggle(bool newValue) // 増幅タブを開く関数
+    private void Update()
     {
-        if(newValue == true) // タブを開くならtrue (閉じる場合は何も関与せず閉じられるように)
-        {
-            if (!BetaUpgradeManager.Instance.IsUpgrade7Completed) // 解放されてるか
-            {
-                Debug.LogWarning("cant open amplfication tab");
-                amplification.SetIsOnWithoutNotify(false); // unityイベントなしでIsOnをfalseに
-            }
-        }
-    }
-
-    private void VaridateMissionToggle(bool newValue) // 試練タブを開く関数
-    {
-        if (newValue == true) // タブを開くならtrue (閉じる場合は何も関与せず閉じられるように)
-        {
-            if (!BetaUpgradeManager.Instance.IsUpgrade8Completed) // 解放されてるか
-            {
-                Debug.LogWarning("cant open mission tab");
-                mission.SetIsOnWithoutNotify(false); // unityイベントなしでIsOnをfalseに
-            }
-        }
-    }
-
-    private void VaridateBankToggle(bool newValue) // 銀行タブを開く関数
-    {
-        if (newValue == true) // タブを開くならtrue (閉じる場合は何も関与せず閉じられるように)
-        {
-            if (!BetaUpgradeManager.Instance.IsUpgrade9Completed) // 解放されてるか
-            {
-                Debug.LogWarning("cant open bank tab");
-                bank.SetIsOnWithoutNotify(false); // unityイベントなしでIsOnをfalseに
-            }
-        }
-    }
-
-    private void VaridateRevolutionToggle(bool newValue) // 革命タブを開く関数
-    {
-        if (newValue == true) // タブを開くならtrue (閉じる場合は何も関与せず閉じられるように)
-        {
-            if (!BetaUpgradeManager.Instance.IsUpgrade10Completed) // 解放されてるか
-            {
-                Debug.LogWarning("cant open revolution tab");
-                revolution.SetIsOnWithoutNotify(false); // unityイベントなしでIsOnをfalseに
-            }
-        }
+        amplification.interactable = BetaUpgradeManager.Instance.IsUpgrade7Completed;
+        mission.interactable = BetaUpgradeManager.Instance.IsUpgrade8Completed;
+        bank.interactable = BetaUpgradeManager.Instance.IsUpgrade9Completed;
+        revolution.interactable = BetaUpgradeManager.Instance.IsUpgrade10Completed;
     }
 }
