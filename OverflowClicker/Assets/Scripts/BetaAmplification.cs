f2ed867ad3783f6de4f33e84431582aa537e8922
nonor102 <105913423+nonor102@users.noreply.github.com>
2025-10-31 21:10:52 +0900

    bityousei and build

--- OverflowClicker/Assets/Scripts/BetaAmplification.cs
+++ OverflowClicker/Assets/Scripts/BetaAmplification.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.Search;
 using UnityEditor;
 
 public class BetaAmplification : MonoBehaviour
